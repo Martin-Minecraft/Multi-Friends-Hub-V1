0a7748c18e0c1e5462958f8c29d202d7c70e53d8
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-06-29 16:03:40 +0200

    Update Multi_LoadingHub.lua

--- Multi_LoadingHub.lua
+++ Multi_LoadingHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/Multi_LoadingHub.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/LoadingHub.lua"))();
