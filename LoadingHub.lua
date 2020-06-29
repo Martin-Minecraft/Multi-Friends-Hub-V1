8a5cdea24591136981fc6b7e7329431c79c0b04f
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-06-29 13:42:29 +0200

    Update LoadingHub.lua

--- LoadingHub.lua
+++ LoadingHub.lua
@@ -1,1 +1,17 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/Loading-UI.lua"))();
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/SKZjdjdj"))();
+
+
+local Gamer = library:CreateSection("FriendsHub");
+
+Gamer:Button("Status: (Working)",function()
+
+end)
+
+Gamer:Button("Multi-Friends-Hub (Loading)",function()
+
+string(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/Start.lua"))();
+
+end)
+
+
+library:Ready();
