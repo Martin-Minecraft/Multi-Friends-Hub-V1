b4e6b9ed94fb2c98c868553632f1c64873f97567
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-07-05 11:35:51 +0200

    Update azure-lib-Hub.lua

--- azure-lib-Hub.lua
+++ azure-lib-Hub.lua
@@ -1,9 +1,12 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
 
 local AimbotTab = Library:CreateTab("Hub", "Bring you to Multi_Friends_Hub", true) 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/Hub.lua"))();
+
+AimbotTab:CreateButton("Multi_Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/Hub.lua"))(); 
 end)
 
-local AimbotTab = Library:CreateTab("Main", "Bring you to Library Board", true) 
+AimbotTab:CreateButton("Main", function() --you dont need "arg" for a button
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/main.lua"))();
 end)
+
