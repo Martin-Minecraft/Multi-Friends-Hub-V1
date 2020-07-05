d43f8f178b36ab464d139c9d000d84286e3f8bcc
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-07-05 11:01:45 +0200

    Create azure-lib-Hub.lua

---
+++ azure-lib-Hub.lua
@@ -0,0 +1,6 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
+
+local AimbotTab = Library:CreateTab("Hub", "This is where you modify the Aimbot", true) 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/Hub.lua"))();
+end)
+
