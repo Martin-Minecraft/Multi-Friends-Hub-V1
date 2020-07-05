ad80f40011a098c725be08b6b37e4b9d33d37712
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-07-05 11:08:57 +0200

    Update azure-lib-Hub.lua

--- azure-lib-Hub.lua
+++ azure-lib-Hub.lua
@@ -4,3 +4,6 @@ local AimbotTab = Library:CreateTab("Hub", "This is where you modify the Aimbot"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/Hub.lua"))();
 end)
 
+local AimbotTab = Library:CreateTab("Main", "This is where you modify the Aimbot", true) 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Martin-Minecraft/Multi-Friends-Hub-V1/master/main.lua"))();
+end)
