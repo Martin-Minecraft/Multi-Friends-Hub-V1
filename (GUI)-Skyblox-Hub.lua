569d4abf874aef9363dc1dcb2122dde892af2fb5
Martin-Minecraft <67168306+Martin-Minecraft@users.noreply.github.com>
2020-07-06 14:34:45 +0200

    Create (GUI)-Skyblox-Hub.lua

---
+++ (GUI)-Skyblox-Hub.lua
@@ -0,0 +1,19 @@
+local library = loadstring(game:HttpGet(('https://pastebin.com/raw/FsJak6AT')))() -- load the library
+
+local w = library:CreateWindow("Skyblox-Hub") -- GUI NAME
+
+local b = w:CreateFolder("Skyblox") -- RANDOM NAME FOLDER
+
+b:Label("hub",Color3.fromRGB(38,38,38),Color3.fromRGB(0,216,111))
+
+b:Button("Skyblox-Trigon",function()
+
+loadstring(game:HttpGet('https://pastebin.com/raw/QniR0TfK'))()
+
+end)
+b:Button("finity-ui-Skyblox",function()
+
+PastebinBanned = false
+loadstring(game:HttpGet("https://system-exodus.com/scripts/Skyblock/Skyblock.lua",true))()
+
+end)
